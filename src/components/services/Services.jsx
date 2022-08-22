d1c9342700056aa4a50056c8cd191e5d9ceb0f82
nerdstarcode <sthivendev@gmail.com>
2022-08-21 22:55:54 -0300

    :construction: Services concluido

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
 import './index.sass'
-
+import {BiCheck} from 'react-icons/bi'
 function Services() {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>O Que Oferto</h5>
+      <h2>Serviços</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Identidade Visual</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Desenvolvimento de logos</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Manual da logo</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Fotografia</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Desenvolvimento</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Portfolio</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Websites</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Manutenção</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Wordpress Manutenção e Updates</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Mockup</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Website Desing</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>App Design</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Responsive Web Desing</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
